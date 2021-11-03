f9dd7c54507e7f8b6ef1c8c37ba5fdbdf33bca06
VeroAnd <averonicajerez@live.com>
2021-11-03 17:55:33 -0300

    agrego carpeta completa

---
+++ script.js
@@ -0,0 +1,81 @@
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
+
+const typeColors = {
+    electric: '#3c43d1',
+    normal: '#B09398',
+    fire: '#E74C3C',
+    water: '#5DADE2',
+    ice: '#A9CCE3',
+    rock: '#9C640C',
+    flying: '#D4E6F1',
+    grass: '#27AE60',
+    psychic: '#CD6155',
+    ghost: '#34495E',
+    bug: '#2ECC71',
+    poison: '#7D3C98',
+    ground: '#935116',
+    dragon: '#1F618D',
+    steel: '#515A5A',
+    fighting: '#D35400',
+    fairy: '#F48FB1',
+    default: '#626567',
+};
+
+const searchPokemon = event => {
+    event.preventDefault();
+    const { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => renderPokemonData(response))
+}
+
+const renderPokemonData = data => {
+    const sprite = data.sprites.front_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `N° ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
+}
+
+const setCardColor = types => {
+    const colorOne = typeColors[types[0].type.name];
+    const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+}
+
+
+const renderPokemonTypes = types => {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = typeColors[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+
+    });
+}
+
+const renderPokemonStats = stats => {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+
+    });
+}
+
